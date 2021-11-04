4af9b407f124734ecf1ff2f9c5ef22db5343879b
chev <111@111.com>
2021-11-04 19:13:50 +0300

    test negative checks

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,8 @@
 from .pages.product_page import BasketPage
+from selenium.webdriver.support.ui import WebDriverWait
 import time
 import pytest
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
@@ -11,21 +12,44 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail)])
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail)
+                    ])
 def test_guest_can_add_product_to_basket(browser,link):
-    print(f"LINK!!!!!: {link}")
     browser.delete_all_cookies()
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"#"http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = BasketPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()
-    time.sleep(5)
+    time.sleep(2)
     price = page.find_price_page()
     print(f"My price!!!!!: {price}")
     product = page.find_product_page()
     print(f"My product!!!!!: {product}")
     page.go_to_basket_page()
-    #time.sleep(3)
+    time.sleep(2)
     page.solve_quiz_and_get_code()
     page.check_product_page(product)
     page.check_price_page(price)
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    basket_page = BasketPage(browser, link)
+    basket_page.open()
+    basket_page.go_to_basket_page()
+    basket_page.should_not_be_success_message()
+
+def test_guest_cant_see_success_messag(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    basket_page = BasketPage(browser, link)
+    basket_page.open()
+    basket_page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    basket_page = BasketPage(browser, link)
+    basket_page.open()
+    basket_page.go_to_basket_page()
+    basket_page.should_disappeared_success_message()
