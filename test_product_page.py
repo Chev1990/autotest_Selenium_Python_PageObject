9f488112fb45edbe3f1898f312913b75ee60e861
chev <111@111.com>
2021-11-05 22:22:47 +0300

    add TestUserAddToBasketFromProductPage

--- test_product_page.py
+++ test_product_page.py
@@ -7,6 +7,7 @@ from .pages.locators import ProductPageLocators
 import time
 import pytest
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -14,28 +15,19 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9",
                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail)
                     ])
 def test_guest_can_add_product_to_basket(browser,link):
     browser.delete_all_cookies()
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"#"http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()
-    time.sleep(2)
-    price = page.find_price_page()
-    print(f"My price!!!!!: {price}")
-    product = page.find_product_page()
-    print(f"My product!!!!!: {product}")
-    page.go_to_basket_page()
-    time.sleep(2)
+    page.add_to_card()
     page.solve_quiz_and_get_code()
-    page.check_product_page(product)
-    page.check_price_page(price)
-
+    page.check_product_name_on_page_and_in_message()
+    page.check_price_on_page_and_in_message()
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -72,12 +64,14 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     basket_page = BasketPage(browser, link)
     basket_page.open()
     basket_page.open_basket()
     basket_page.is_basket_empty()
+
 
 @pytest.mark.login
 class TestUserAddToBasketFromProductPage():
